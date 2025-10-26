1a009188493fcc554ff7f0588413092ce3f85b87
Gopal Shukla <gopalshukla0018@gmail.com>
2025-10-26 23:57:05 +0530

    fix: get notes to get all notes and feat: add get notes controller

--- server/controllers/createNotes.controller.js
+++ server/controllers/createNotes.controller.js
@@ -1,4 +1,5 @@
 import { Note } from "../models/note.model.js";
+import mongoose from "mongoose";
 
 // create notes controller
 export const createNote = async (req, res) => {
@@ -27,15 +28,42 @@ export const createNote = async (req, res) => {
   }
 };
 
-// get course controller
+// get notes controller
 
-export const getNotes = async (req, res) => {
+export const getnotesAll = async (req, res) => {
   try {
     const notes = await Note.find({}).sort({ createdAt: -1 });
     return res.status(200).json({
       notes,
     });
   } catch (error) {
-    console.log("error in getNotes controller", error);
+    console.log("error in getnotesAll controller", error);
+  }
+};
+
+// get notes by id
+
+export const getNotesById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid note ID",
+      });
+    }
+    const note = await Note.findById(id)
+    if (!note) {
+      return res.staus(404).json({
+        success: false,
+        message: "note not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: note,
+    });
+  } catch (error) {
+    console.log("error in getnotesid controller", error);
   }
 };
--- server/routes/notes.route.js
+++ server/routes/notes.route.js
@@ -1,17 +1,19 @@
-import   express  from "express";
-import { createNote, getNotes } from "../controllers/createNotes.controller.js";
+import express from "express";
+import {
+  createNote,
+  getnotesAll,
+  getNotesById,
+} from "../controllers/createNotes.controller.js";
 
 const router = express.Router();
 
-
-
 //get all notes route
 
-
 // create notes route
-router.route("/").post(createNote)
+router.route("/").post(createNote);
 
-router.route("/").get(getNotes)
+router.route("/").get(getnotesAll);
 
+router.route("/:id").get(getNotesById);
 
 export default router;
